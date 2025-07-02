d9ccdc41c71267fa3564b1024c0d613e36c47235
Nullskulls <Nullskulls@proton.me>
2025-07-02 19:49:59 +0300

    v1.0.4 Quality of life improvement.

--- Source/Draw.py
+++ Source/Draw.py
@@ -1,16 +1,32 @@
+letters = ['no','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 def draw(board="None", orientation="White"):
     if board == "None":
         raise TypeError("Board cannot be None")
+    i = 8
+    j = 0
     if orientation == "White":
         for row in board:
+            print(f"{i}  ", end='')
             for col in row:
                 print(col, end="    ")
             print(end="\n \n")
+            i-=1
     if orientation == "Black":
         for row in board[::-1]:
+            print(f"{i}  ", end='')
             for col in row:
                 print(col, end="    ")
             print(end="\n \n")
+            i -= 1
+    draw_bottom_border()
+
+def draw_bottom_border():
+    for i in range(9):
+        if i == 0:
+            print("*  ", end='')
+            continue
+        print(f"{letters[i]}    ", end='')
+    print()
 
 
 if __name__ == "__main__":
